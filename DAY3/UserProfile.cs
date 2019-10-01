b111da8c171e08b8ff335962c72324b844aac6bb
Mija Zake <mija.zake@gmail.com>
2019-10-01 19:32:18 +0300

    Added exceptions, logic and DAY3

---
+++ DAY3/UserProfile.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAY3
+{
+    class UserProfile
+    {
+        public const char MALE = 'M';
+        public const char FEMALE = 'F';
+
+        public enum Genders
+        {
+            Male,
+            Female
+        }
+
+        public string FullName { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public Genders Gender { get; set; }
+        //public char Gender, if no enum
+
+        public UserProfile(string fullName, DateTime birthDate, Genders gender)
+        {
+            FullName = fullName;
+            BirthDate = birthDate;
+            Gender = gender;
+        }
+
+
+    }
+}
--- Exceptions/Program.cs
+++ Exceptions/Program.cs
@@ -10,8 +10,47 @@ namespace Exceptions
     {
         static void Main(string[] args)
         {
+            try
+            {
+                int num1 = GetNumber("Enter number A: ");
+                int num2 = GetNumber("Enter number B: ");
 
+                Console.WriteLine("Division = {0}", Calculator.Divide(num1, num2));
+            }
+            catch(CalculatorException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch
+            {
+                Console.WriteLine("Neparedzeta kluda!");
+            }
 
+            Console.Read();
+        }
+
+        static int GetNumber(string text)
+        {
+            Console.Write(text); //enter num1, enter num2
+            string input = Console.ReadLine();
+
+            //1var
+            //if(int.TryParse(input, out int result))
+            // {
+            //    return result;
+            //}
+
+            //2var
+            try
+            {
+                int result = int.Parse(input);
+                return result;
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Invalid number!");
+                return GetNumber(text);
+            }
         }
     }
 }
