0471dcc3e78a91d473e600631aaeb306e39090d1
Mija Zake <mija.zake@gmail.com>
2019-10-03 20:48:26 +0300

    New project - DAY4 ABSTRACTIONS

---
+++ DAY4_ABSTRACTION/BasePlayer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAY4_ABSTRACTION
+{
+    abstract class BasePlayer : IPlayer
+    {
+        string Name;
+        int CurrentGuess;
+
+        bool IsNumberGuessed(int number)
+        {
+
+        }
+
+        abstract string GetName()
+        {
+
+        }
+
+        public int GuessNumber()
+        {
+
+        }
+    }
+}
---
+++ DAY4_ABSTRACTION/Game.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAY4_ABSTRACTION
+{
+    class Game
+    {
+        int CurrentNumber;
+        IPlayer PlayerOne;
+        IPlayer PlayerTwo;
+
+        void StartNewGame()
+        {
+            //Random rnd = new Random();
+            //int[] numbers = { };
+        }
+
+        void Loop()
+        {
+            while(true)
+            {
+                Console.Write("Guess the number: ");
+                CurrentNumber = int.Parse(Console.ReadLine());
+            }
+        }
+
+        void PlayerTurn(IPlayer player)
+        {
+
+        }
+    }
+}
