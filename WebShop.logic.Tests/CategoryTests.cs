3748b55248ae6e03c7a5fcad6558ea16984714a4
Mija Zake <mija.zake@gmail.com>
2019-10-19 15:13:42 +0300

    WebShop

--- WebShop.logic.Tests/CategoryTests.cs
+++ WebShop.logic.Tests/CategoryTests.cs
@@ -7,8 +7,31 @@ namespace WebShop.logic.Tests
     public class CategoryTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestGetAll()
         {
+            CategoryManager manager = new CategoryManager();
+            manager.Seed();
+            
+            var result = manager.GetAll();
+
+            Assert.AreEqual("Big category", result[0].Title);
+            Assert.AreEqual(2, result.Count);
+        }
+
+        [TestMethod]
+        public void TestGet()
+        {
+            CategoryManager manager = new CategoryManager();
+            manager.Seed();
+
+            var category1 = manager.Get(1);
+            var category2 = manager.Get(2);
+
+            Assert.AreEqual(1, category1.Id);
+            Assert.AreEqual("Big category", category1.Title);
+            Assert.AreEqual(2, category2.Id);
+            Assert.AreEqual("Small category", category2.Title);
+            Assert.AreEqual(1, category2.CategoryId);
         }
     }
 }
--- WebShop.logic.Tests/ItemTests.cs
+++ WebShop.logic.Tests/ItemTests.cs
@@ -46,6 +46,24 @@ namespace WebShop.logic.Tests
         {
             ItemManager manager = new ItemManager();
             manager.Seed();
+
+            manager.Update(new Item() { 
+            Id = 2,
+            Price = 30,
+            Title = "New Item",
+            Description = "New description",
+            Photo = "New Photo",
+            CategoryId = 2
+            });
+
+            var item = manager.Get(2);
+
+            Assert.AreEqual(2, item.Id);
+            Assert.AreEqual(30, item.Price);
+            Assert.AreEqual("New Item", item.Title);
+            Assert.AreEqual("New description", item.Description);
+            Assert.AreEqual("New Photo", item.Photo);
+            Assert.AreEqual(2, item.CategoryId);
         }
 
         [TestMethod]
@@ -53,6 +71,12 @@ namespace WebShop.logic.Tests
         {
             ItemManager manager = new ItemManager();
             manager.Seed();
+
+            manager.Delete(1);
+
+            var deletedItem = manager.Get(1);
+
+            Assert.IsNull(deletedItem);
         }
 
         [TestMethod]
@@ -60,6 +84,26 @@ namespace WebShop.logic.Tests
         {
             ItemManager manager = new ItemManager();
             manager.Seed();
+
+            var item1 = manager.Get(1);
+            var item2 = manager.Get(2);
+            var item3 = manager.Get(3);
+
+            Assert.AreEqual(1, item1.Id);
+            Assert.AreEqual(10, item1.Price);
+            Assert.AreEqual("Item 1", item1.Title);
+            Assert.AreEqual("Desc 1", item1.Description);
+            Assert.AreEqual("Photo 1", item1.Photo);
+            Assert.AreEqual(2, item1.CategoryId);
+
+            Assert.AreEqual(2, item2.Id);
+            Assert.AreEqual(20, item2.Price);
+            Assert.AreEqual("Item 2", item2.Title);
+            Assert.AreEqual("Desc 2", item2.Description);
+            Assert.AreEqual("Photo 2", item2.Photo);
+            Assert.AreEqual(2, item2.CategoryId);
+
+            Assert.IsNull(item3);
         }
     }
 }
